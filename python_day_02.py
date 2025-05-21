d63ec46c33233a234570b81c137f7e645eeaabd3
Vikas Chand <121432906+Vikaskumain@users.noreply.github.com>
2025-05-21 09:24:24 +0530

    Add files via upload

---
+++ python_day_02.py
@@ -0,0 +1,128 @@
+# Filename: python_day_02.py
+# Subtraction                                               
+# This code performs basic arithmetic operations
+# Addition  
+a=20
+b=10
+c=a+b
+print("Addition of a and b is", c)
+# Subtraction
+a=20
+b=10
+c=a-b
+
+print("Subtraction of a and b is", c)
+# Multiplication        
+a=20
+b=10  
+c=a*b
+print("Multiplication of a and b is", c)
+# Division  
+a=20
+b=10
+c=a/b
+print("Division of a and b is", c)
+# Modulus
+a=20
+b=10
+c=a%b
+print("Modulus of a and b is", c)
+# Floor Division
+a=20
+b=10
+c=a//b
+print("Floor Division of a and b is", c)
+# Exponentiation
+a=20
+b=10
+c=a**b
+print("Exponentiation of a and b is", c)
+# Comparison
+a=20
+b=10
+c=a==b
+print("Comparison of a and b is", c)
+# Logical Operators
+a=True
+b=False
+c=a and b
+print("Logical Operators of a and b is", c)
+# Identity Operators
+a=20
+b=10        
+c=a is b
+print("Identity Operators of a and b is", c)
+# Bitwise Operators
+a=20
+b=10  
+c=a&b
+print("Bitwise Operators of a and b is", c)
+a=20
+b=10
+c=a if a>b else b
+print("Ternary Operator of a and b is", c)
+# String Operators
+a="Hello"
+b="World"   
+c=a+b
+print("String Operators of a and b is", c)
+# List Operators
+a=[1,2,3]
+b=[4,5,6]
+c=a+b
+print("List Operators of a and b is", c)
+# Tuple Operators
+a=(1,2,3)
+b=(4,5,6)
+c=a+b
+print("Tuple Operators of a and b is", c)
+# Dictionary Operators
+a={"a":1,"b":2}
+b={"c":3,"d":4}
+c={**a, **b}
+print("Dictionary Operators of a and b is", c)
+# Set Operators
+a={1,2,3}
+b={4,5,6}
+c=a|b
+print("Set Operators of a and b is", c)
+# String Formatting     
+a="Hello"
+b="World"
+c="{} {}".format(a,b)
+print("String Formatting of a and b is", c)
+# String Concatenation
+a="Hello"
+b="World"
+c=a+b
+print("String Concatenation of a and b is", c)
+# String Repetition
+a="Hello"
+b=3
+c=a*b
+print("String Repetition of a and b is", c)
+# String Slicing
+a="Hello World"
+b=a[0:5]
+print("String Slicing of a and b is", b)
+# String Length
+a="Hello World"
+b=len(a)
+print("String Length of a and b is", b)
+# String Count
+a="Hello World"
+b=a.count("o")
+
+print("String Count of a and b is", b)
+# String Find
+a="Hello World"
+b=a.find("o")
+print("String Find of a and b is", b)
+# String Replace
+a="Hello World"
+b=a.replace("o","O")
+print("String Replace of a and b is", b)
+# String Split
+a="Hello World"
+b=a.split(" ")
+
