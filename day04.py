56aba1fbdb2666cba32bf4cb03546d8ff1d09df2
Vikas Chand <121432906+Vikaskumain@users.noreply.github.com>
2025-05-23 10:23:22 +0530

    Add files via upload

---
+++ day04.py
@@ -0,0 +1,79 @@
+We study if condtional statements with python 
+
+We study conditional statements with Python
+
+Marks = int(input("How many marks did you get? "))
+
+if (Marks >= 90):
+    print(f"You are in A+ Grade. You got {Marks} marks.")
+elif (Marks < 40):
+    print(f"You failed because you got only {Marks} marks.")
+else:
+    print(f"You passed with {Marks} marks.")
+
+marks = [14,45,12,78,90,100,23,56,67,89]
+marks.sort()
+print(marks)
+marks.append(100)
+print(marks)
+marks.pop()
+print(marks)
+list and tuple and strings
+list 
+list we can change its called to mutable
+list stored only square brackets like this []
+list sliceing
+these are all the list methods
+collection =["vikas ","kumain","chand","raj","suresh","kumar"]
+print(collection)
+collection[2]="rajkumar"
+print(collection)
+collection[0]="vikas kumar"
+print(collection)
+print(collection[-5])
+collection.reverse()
+print(collection)
+collection.sort()
+print(collection)
+collection.append("rajkumar")     
+print(collection)
+collection.pop()
+print(collection)
+collection.remove("rajkumar")
+# print(collection)
+collection.insert(2,"rajkumar")
+print(collection)
+collection.clear()
+print(collection)
+collection.extend(["rajkumar","suresh"])
+print(collection)
+extend is used to add multiple values in list for last 
+collection.count("rajkumar")
+
+
+
+string = "vikas kumar"
+print(string[-4])
+it count all numbers of strings like black space and all and return the index each letter
+
+string[0]="V"
+print(string)
+strings we can not change its called to immutable,
+
+tuple
+tuple we can not change its called to immutable     
+names =("vikas ","kumain","chand","raj","suresh","kumar")
+print(names)
+print(names[2])
+it also count all words of tuple each and every words not a letter then retruns 
+names[2]="rajkumar"
+print(names)
+tuple object does not support item assignment
+tuple stored only brackets like this () 
+
+
+
+
+
+
+
